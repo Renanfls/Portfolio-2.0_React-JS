79150fd59888b4ec9abeeac71cffee86962be202
Renanfls <contato.renanfabricio@gmail.com>
2023-05-19 20:41:48 -0300

    Update navbar

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,65 +1,22 @@
 'use client'
-import { Button, Container, Nav, Navbar, Offcanvas } from "react-bootstrap"
+import { Button, Nav, Navbar } from "react-bootstrap"
 
 function Header() {
     return (
-        <header>
-            <Navbar  expand="lg">
-                <Container>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="d-flex justify-content-between me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <div className="d-flex flex-row justify-start">
-                            <Nav.Link href="#about" className="link">Sobre</Nav.Link>
-                            <Nav.Link href="#experience" className="link">Experiência</Nav.Link>
-                            <Nav.Link href="#" className="link">Projetos</Nav.Link>
-                            <Nav.Link href="#" className="link">Contato</Nav.Link>
-                        </div>
-                        <Button variant="outline-success resumeButton" href="file:///C:/Users/renan/Downloads/Profile.pdf">Resumo</Button>
+        <header className="fixed-top">
+            <Navbar collapseOnSelect expand="lg" variant="dark">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#about" className="link">Sobre</Nav.Link>
+                        <Nav.Link href="#experience" className="link">Experiência</Nav.Link>
+                        <Nav.Link href="#" className="link">Projetos</Nav.Link>
+                        <Nav.Link href="#" className="link">Contato</Nav.Link>
                     </Nav>
-                    </Navbar.Collapse>
-                </Container>
+                    <Button className="resumeButton" href="file:///C:/Users/renan/Downloads/Profile.pdf">Resumo</Button>
+                </Navbar.Collapse>
             </Navbar>
-
-
-
-
-
-{/*             {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-                <Navbar className="mb-3">
-                    <Container fluid>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                Offcanvas
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-between flex-grow-1 py-5">
-                                    <div className="d-flex flex-row justify-start">
-                                        <Nav.Link href="#about" className="link">Sobre</Nav.Link>
-                                        <Nav.Link href="#experience" className="link">Experiência</Nav.Link>
-                                        <Nav.Link href="#" className="link">Projetos</Nav.Link>
-                                        <Nav.Link href="#" className="link">Contato</Nav.Link>
-                                    </div>
-                                    <Button variant="outline-success resumeButton" href="file:///C:/Users/renan/Downloads/Profile.pdf">Resumo</Button>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-            ))} */}
         </header>
-
     )
 }
 
